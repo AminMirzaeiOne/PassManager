083236c9cfb28efa94a0f0dcd5c2239a2468ff02
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-08-27 11:13:10 +0330

    Updata PasswordItem Class Base Code

--- PassManager/Domain/Entities/PasswordItem.cs
+++ PassManager/Domain/Entities/PasswordItem.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace PassManager.Domain.Entities
 {
-    internal class PasswordItem
+    public class PasswordItem
     {
+        public int Id { get; set; }
+        public string? Title { get; set; }            // e.g., "Gmail"
+        public string? Username { get; set; }         // account username
+        public string? EncryptedPassword { get; set; } // encrypted bytes base64
+        public string? Notes { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
