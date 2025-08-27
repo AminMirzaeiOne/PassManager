9dfb588644833b1148fd60afdb534b9eb8bf99fc
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-08-27 11:18:02 +0330

    - Add EntityFrameworkCore To Project And Update AppDBContext Class

--- PassManager/Infrastructure/Data/AppDBContext.cs
+++ PassManager/Infrastructure/Data/AppDBContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using PassManager.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace PassManager.Infrastructure.Data
 {
-    internal class AppDBContext
+    public class AppDBContext : DbContext
     {
+        public DbSet<PasswordItem> PasswordItems { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.PasswordHash).HasColumnType("BLOB");
+            modelBuilder.Entity<User>()
+                .Property(u => u.PasswordSalt).HasColumnType("BLOB");
+            modelBuilder.Entity<User>()
+                .Property(u => u.KeySalt).HasColumnType("BLOB");
+        }
     }
 }
