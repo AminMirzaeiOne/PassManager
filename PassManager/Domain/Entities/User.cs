97696e10275b675e142742c35f240b082453cafa
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-08-27 11:14:29 +0330

    - Update User Class Base Code

--- PassManager/Domain/Entities/User.cs
+++ PassManager/Domain/Entities/User.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace PassManager.Domain.Entities
 {
-    internal class User
+    public class User
     {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+
+        // Authentication (PBKDF2 hash)
+        public byte[]? PasswordHash { get; set; }
+        public byte[]? PasswordSalt { get; set; }
+        public int Iterations { get; set; }
+
+        // separate salt for deriving AES key
+        public byte[]? KeySalt { get; set; }
     }
 }
