c4dbd061886ded165ea89923ff8c3ca807921980
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-08-27 11:15:35 +0330

    - Update IPasswordReposiotry Base Code

--- PassManager/Domain/Interfaces/IPasswordRepository.cs
+++ PassManager/Domain/Interfaces/IPasswordRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PassManager.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace PassManager.Domain.Interfaces
 {
-    internal interface IPasswordRepository
+    public interface IPasswordRepository
     {
+        Task<IEnumerable<PasswordItem>> GetAllAsync();
+        Task<PasswordItem> GetByIdAsync(int id);
+        Task AddAsync(PasswordItem item);
+        Task UpdateAsync(PasswordItem item);
+        Task DeleteAsync(int id);
     }
 }
