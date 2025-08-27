4e61e9d9e060c1f94083c3fab9b3f06fc15ba771
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-08-27 11:21:00 +0330

    - Update PasswordReposiotry Class Base Code

--- PassManager/Infrastructure/Repositories/PasswordRepository.cs
+++ PassManager/Infrastructure/Repositories/PasswordRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PassManager.Domain.Entities;
+using PassManager.Domain.Interfaces;
+using PassManager.Infrastructure.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,31 @@ using System.Threading.Tasks;
 
 namespace PassManager.Infrastructure.Repositories
 {
-    internal class PasswordRepository
+    public class PasswordRepository : IPasswordRepository
     {
+        private readonly AppDBContext _ctx;
+        public PasswordRepository(AppDBContext ctx) => _ctx = ctx;
+
+        public async Task AddAsync(PasswordItem item)
+        {
+            _ctx.PasswordItems.Add(item);
+            await _ctx.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var e = await _ctx.PasswordItems.FindAsync(id);
+            if (e != null) { _ctx.PasswordItems.Remove(e); await _ctx.SaveChangesAsync(); }
+        }
+        public async Task<IEnumerable<PasswordItem>> GetAllAsync()
+            => await _ctx.PasswordItems.AsNoTracking().ToListAsync();
+
+        public async Task<PasswordItem> GetByIdAsync(int id)
+            => await _ctx.PasswordItems.FindAsync(id);
+
+        public async Task UpdateAsync(PasswordItem item)
+        {
+            _ctx.PasswordItems.Update(item);
+            await _ctx.SaveChangesAsync();
+        }
     }
 }
